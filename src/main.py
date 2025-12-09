1ae781963573e3fc5791d912b4409c557b28940f
adee0210 <anhduc02102004@gmail.com>
2025-12-10 01:21:50 +0700

    update

--- src/main.py
+++ src/main.py
@@ -18,14 +18,3 @@ from utils.selenium_util import SeleniumUtil
 from vn.candlestick_data_pipeline.extract.historical_extract.etf_1minute_historical_extract import (
     ETF1MinuteHistoricalExtract,
 )
-
-# sele = SeleniumUtil()
-# data = sele.get_data_by_tag_name(
-#     url="https://tvc4.investing.com/4379fbb3b2cf9327a768dbfde5d69d1d/1761556750/52/52/110/history?symbol=E1VFVN30&resolution=1&from=1609459200&to=1672531199",
-#     tag_name="body",
-# )
-# with open("test.json", "w") as f:
-#     json.dump(data, f)
-
-etf = ETF1MinuteHistoricalExtract()
-etf.extract_logic()
