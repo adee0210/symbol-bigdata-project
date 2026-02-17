bb42ace2dbdd4fbba3c09f4ab34dd215dcdbc779
adee0210 <anhduc02102004@gmail.com>
2026-02-17 08:58:01 +0700

    update

--- src/projects/ckvn/stock/candlestick/etl/extract/historical_extract.py
+++ src/projects/ckvn/stock/candlestick/etl/extract/historical_extract.py
@@ -1,13 +1,17 @@
 from datetime import datetime, timezone
+from logging import Logger
 import cloudscraper
 from ckvn.stock.candlestick.configs.config import SYMBOL
 from ckvn.stock.candlestick.etl.extract.historical_extract_interface import (
     HistoricalExtractInterface,
 )
 
+from common.configs.logging_config import LoggingConfig
+
 
 class HistoricalExtract(HistoricalExtractInterface):
     def __init__(self) -> None:
+        self.logger = LoggingConfig.logging_config("HistoricalStockCandlestickExtract")
         self.investing_api = str(SYMBOL["investing_api"])
         self.symbol = [SYMBOL[s].keys() for s in SYMBOL.keys()]
 
@@ -20,6 +24,7 @@ class HistoricalExtract(HistoricalExtractInterface):
             return data
 
         except Exception as e:
+            self.logger.info(f"Error: {str(e)}")
             raise
 
     def historical_extract_logic(self):
