17e25fd8077871ee6d4cd05ccea087cb63a3648f
adee0210 <anhduc02102004@gmail.com>
2026-04-08 00:57:14 +0700

    update

--- src/projects/ckvn/stock/candlestick/etl/extract/historical/historical_extract.py
+++ src/projects/ckvn/stock/candlestick/etl/extract/historical/historical_extract.py
@@ -1,46 +1,36 @@
 from datetime import datetime, timezone
+import json
 from logging import Logger
-import cloudscraper
-from ckvn.stock.candlestick.configs.config import SYMBOL
+from time import sleep
+from ckvn.stock.candlestick.configs.config import SYMBOL, SYMBOL_API
 from ckvn.stock.candlestick.etl.extract.historical.historical_extract_interface import (
     HistoricalExtractInterface,
 )
 
-from selenium import webdriver
-from selenium.webdriver.chromium.service import ChromiumService
-from selenium.webdriver.chrome.options import Options
-
 from common.configs.logging_config import LoggingConfig
+from common.utils.selenium_util import SeleniumUtil
 
 
 class HistoricalExtract(HistoricalExtractInterface):
     def __init__(self) -> None:
-        self.logger = LoggingConfig.logging_config("HistoricalStockCandlestickExtract")
-        self.investing_api = str(SYMBOL["investing_api"])
+        # self.logger = LoggingConfig.logging_config("HistoricalStockCandlestickExtract",)
+        self.investing_api = str(SYMBOL_API["investing_api"])
+        self.selenium_util = SeleniumUtil()
         self.symbol = [SYMBOL[s].keys() for s in SYMBOL.keys()]
 
-    def logic(self):
-        def create_chromium_driver():
-
-        driver_path = r"/snap/bin/chromium.chromedriver"
-        chromium_option = Options()
-        chromium_option.add_argument("--no-sandbox")
-        chromium_option.add_argument("--disable-dev-shm-usage")
-        chromium_option.add_argument("--disable-gpu")
-        chromium_option.add_argument("--disable-blink-features=AutomationControlled")
-        chromium_option.add_argument("--no-first-run")
-        chromium_option.add_argument("--disable-default-apps")
-        chromium_option.add_argument("--disable-extensions")
-        # chromium_option.add_argument("--headless=new")  # Disabled to show browser UI
-        return webdriver.Chrome(
-            service=ChromiumService(driver_path), options=chromium_option
+    def historical_extract(self):
+        url = "https://tvc4.investing.com/4379fbb3b2cf9327a768dbfde5d69d1d/1761556750/52/52/110/history?symbol=vcb&resolution=1&from=1738256400&to=1767114000"
+        self.selenium_util.extract_element_from_url_to_json(
+            url, by_element="tag name", element_name="body"
         )
 
-            
-
-
-    def historical_extract_logic(self):
+    def storage_historical_extract_data(self):
         return None
 
-    def historical_extract(self):
-        return None
+
+test = HistoricalExtract()
+test.historical_extract()
+# test.historical_extract_logic()
+# end_time = datetime(year=2025, month=12, day=31).timestamp()
+# start_time = datetime(year=2025, month=1, day=31).timestamp()
+# print(end_time, start_time)
--- src/projects/ckvn/stock/candlestick/etl/extract/historical/historical_extract_interface.py
+++ src/projects/ckvn/stock/candlestick/etl/extract/historical/historical_extract_interface.py
@@ -3,10 +3,6 @@ from abc import ABC, abstractmethod
 
 class HistoricalExtractInterface(ABC):
     @abstractmethod
-    def historical_extract_logic(self):
-        pass
-
-    @abstractmethod
     def historical_extract(self):
         pass
 
