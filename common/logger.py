132fe320c464b42463a320ae59991e4755d38dc0
adee0210 <anhduc02102004@gmail.com>
2026-06-02 23:05:25 +0700

    update

--- common/logger.py
+++ common/logger.py
@@ -4,7 +4,7 @@ class Logger:
     def logger(log_name,log_level = logging.INFO):
         logger = logging.getLogger(log_name)
         logger.setLevel(log_level)
-        file_handler = logging.FileHandler('app.log')
+        file_handler = logging.FileHandler(log_name + '.log')
         file_handler.setLevel(log_level)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         file_handler.setFormatter(formatter)
--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-a = input( "Enter a number: " )
-
-def test(i):
-    for i in range(0, 10):
-        print(i)
-
-test(a)
+print("Hello, World!")
